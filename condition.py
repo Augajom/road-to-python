8d5f66ffa72bd5f3ee7578ed5bd4038babc80e42
AujomK <aujom2541@gmail.com>
2025-10-08 13:10:16 +0700

    condition

---
+++ condition.py
@@ -0,0 +1,13 @@
+score = int(input("กรอกคะแนนของคุณ: "))
+print("คะแนนของคุณ:", score)
+
+if score >= 80:
+    print("เกรด A")
+elif score >= 70:
+    print("เกรด B")
+elif score >= 60:
+    print("เกรด C")
+elif score >= 50:
+    print("เกรด D")
+else:
+    print("เกรด F")
--- valiable.py
+++ valiable.py
@@ -2,8 +2,23 @@ string = "Hello, World!"
 number = 10
 float_num = 3.14
 boolean = True
+a, b, c = 1, 2, 3
 
+print(a, b, c)
 
 name = input("กรอกชื่อของคุณ: ")
+year = int(input("กรอกปีเกิดของคุณ: "))
 
 print("สวัสดี", name)
+print("เกิดในปี", year)
+
+# input เก็บเป็น string เสมอ
+# ต้องแปลงก่อนถ้าจะเอาไปใช้คำนวณ
+#print("ปีนี้คุณอายุ", 2568 - int(year), "ปี") # แปลงแค่ส่วนนี้ ถ้าอยากให้เป็น int ตั้งแต่ต้นให้เพิ่ม int() ที่ตัวแปลเลย
+print("ปีนี้คุณอายุ", 2568 - year, "ปี")
+
+print(type(name))
+print(type(year))
+
+# ตัวแปลงทั้งหมด
+# int() float() str() bool()
